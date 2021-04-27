acd7d78d892bc64f0e058d545f6e21f7f9c111fc
danro <danro@MSI>
2021-04-26 18:48:41 -0700

    Add Entry Java file

---
+++ AddEntry/src/AddEntry.java
@@ -0,0 +1,26 @@
+
+//GUI for Adding Password entry (Demo 1)
+// Created by Daniel Rodriguez Zanuto
+import java.util.Scanner;		//Keyboard inputs
+
+
+public class AddEntry {
+
+	  
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		System.out.println("Please enter the platform that is attached to the password you are storing");
+		String platform = input.nextLine();
+			
+			
+		System.out.println("Please enter the desired password");
+		String password = input.nextLine();
+			
+	  
+	    
+	  
+	  
+	  
+}
+}
