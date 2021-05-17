aa211da9351b50e26ec6941523f390e38e86e219
Edwin <83030257+Edwin-Singh@users.noreply.github.com>
2021-05-17 01:07:24 -0700

    Add files via upload

---
+++ softwareEngineering.java
@@ -0,0 +1,15 @@
+import java.util.Scanner; 
+
+public class SoftwareEngineering {
+
+  class main {
+    public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in); // Creates Scanner
+    	System.out.println("Search for Entry: ") // Asks User For Their Search Input
+    	Object entry;
+		String searchEntry = ((Scanner) userEntry).nextLine(); // Takes User Input
+		System.out.println("Your entry is" searchEntry) // Outputs What Was Searched
+    	
+        
+}
+} }
